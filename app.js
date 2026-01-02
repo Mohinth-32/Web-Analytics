c6103bd077ff5c26e0b15f052db6446cc091155c
Mohinth-32 <727821tuit101@skct.edu.in>
2026-01-02 15:18:11 +0530

    init

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ app.get("/analytics/summary", async (req, res) => {
   }
 });
 
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
